03d53d053bb19e5baab0bc0351d2ee63ff823571
fin del kind <fin+github@xbhd.org>
2010-12-10 17:59:53 +0100

    fix path & import

--- src/Classes/StatusOverlay.h
+++ src/Classes/StatusOverlay.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <Sparrow.h>
 
 @interface StatusOverlay : SPSprite {
 	
--- src/Classes/StatusOverlay.m
+++ src/Classes/StatusOverlay.m
@@ -24,7 +24,7 @@
 - (id)init {
     if (self = [super init]) {
         [self setupSprite];    
-		gravityBarWhite = [SPImage imageWithContentsOfFile:@""]
+		gravityBarWhite = [SPImage imageWithContentsOfFile:@""];
     }
     return self;
 }
